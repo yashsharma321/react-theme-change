ec47ec617037afb89ec1ce902896590f3e1cb39c
Yash Sharma <yash.arkedia@gmail.com>
2023-11-22 08:23:58 +0000

    on load same theme

--- theme-change/src/components/DarkMode/DarkMode.js
+++ theme-change/src/components/DarkMode/DarkMode.js
@@ -6,10 +6,17 @@ import "./DarkMode.css";
 const DarkMode = () => {
   const setDarkMode = () => {
     document.querySelector("body").setAttribute("data-theme", "dark");
+    localStorage.setItem("selectedTheme", "dark");
   };
   const setlightMode = () => {
     document.querySelector("body").setAttribute("data-theme", "light");
+    localStorage.setItem("selectedTheme", "light");
   };
+
+  const selectedTheme = localStorage.getItem("selectedTheme");
+  if (selectedTheme === "dark") {
+    setDarkMode();
+  }
   const toggleTheme = (e) => {
     if (e.target.checked) setDarkMode();
     else setlightMode();
@@ -21,6 +28,7 @@ const DarkMode = () => {
         type="checkbox"
         id="darkmode-toggle"
         onChange={toggleTheme}
+        defaultChecked={selectedTheme === "dark"}
       />
       <label className="dark_mode_label" for="darkmode-toggle">
         <Sun />
