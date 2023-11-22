9c3d036e6630d77816e680332d44487fb6d6c8c7
Yash Sharma <yash.arkedia@gmail.com>
2023-11-22 08:15:46 +0000

    themeChange

--- theme-change/src/App.js
+++ theme-change/src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+import DarkMode from "./components/DarkMode/DarkMode";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <DarkMode />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
